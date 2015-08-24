4c25b86b87f4f088c12e07194e9eaa760240bfff
Jared Hyers <jared@laicos.com>
2015-08-24 11:47:19 -0400

    Step 5
    More filtering options
    Alpha
    New
    Old

--- js/controllers.js
+++ js/controllers.js
@@ -3,17 +3,22 @@ angular.module('phonecatApp', [])
    $scope.phones= [
       {
          name: 'Nexus S',
-         snippet: 'Fast just got faster with Nexus S.'
+         snippet: 'Fast just got faster with Nexus S.',
+         age: 1
       },
       {
          name: 'Motorola X00m with Wi-Fi',
-         snippet: 'The Next, Next Generation tablet'
+         snippet: 'The Next, Next Generation tablet',
+         age: 2
       },
       {
          name: 'Motorola X00m',
-         snippet: 'The Next, Next Generation tablet'
+         snippet: 'The Next, Next Generation tablet',
+         age:3
       }
    ];
+
+   $scope.orderProp = 'age';
 
    $scope.name = "Jared Phone Gallery App";
 });

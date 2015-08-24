f9f2d627c94a93ce243db9ca17fe7c506a055a52
Jared Hyers <jared@laicos.com>
2015-08-24 12:13:17 -0400

    Add phones
    add phone lib with .json files of phones and information

--- js/controllers.js
+++ js/controllers.js
@@ -1,22 +1,8 @@
 angular.module('phonecatApp', [])
-.controller('PhoneListCtrl', function($scope){
-   $scope.phones= [
-      {
-         name: 'Nexus S',
-         snippet: 'Fast just got faster with Nexus S.',
-         age: 1
-      },
-      {
-         name: 'Motorola X00m with Wi-Fi',
-         snippet: 'The Next, Next Generation tablet',
-         age: 2
-      },
-      {
-         name: 'Motorola X00m',
-         snippet: 'The Next, Next Generation tablet',
-         age:3
-      }
-   ];
+.controller('PhoneListCtrl', function($scope, $http){
+   $http.get('phones/phones.json').success(function(data){
+      $scope.phones = data;
+   });
 
    $scope.orderProp = 'age';
 

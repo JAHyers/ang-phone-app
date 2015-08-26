43c33121845d2fbe86fd402f4858807e0d4b008a
Jared Hyers <jared@laicos.com>
2015-08-26 10:16:18 -0400

    step 7
    ngView, ngRoute, templating

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,25 @@
+var phonecatControllers = angular.module('phonecatControllers',[]);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+   function($scope, $http) {
+      $http.get('js/phones/phones.json').success(function(data){
+         $scope.phones = data;
+      });
+      $scope.orderProp = 'name';
+
+      $scope.title = {
+         name: "Phone Gallery App",
+         dev: "Jared Hyers",
+         version: '1.0',
+         tech: "Angular, HTML, CSS, Bootstrap",
+      }
+   }]);
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+   function($scope, $routeParams){
+      $scope.phoneId = $routeParams.phoneId;
+   }]);
+
 angular.module('phonecatApp', [])
 .controller('PhoneListCtrl', function($scope, $http){
-   $http.get('js/phones/phones.json').success(function(data){
-      $scope.phones = data;
+
    });
-
-   $scope.orderProp = 'name';
-
-   $scope.title = {
-      name: "Phone Gallery App",
-      dev: "Jared Hyers",
-      version: '1.0',
-      tech: "Angular, HTML, CSS, Bootstrap",
-   }
-
-});
